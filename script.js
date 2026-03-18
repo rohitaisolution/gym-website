96e055885e76b0404f83b00333fa81e1f1fd327f
rohitaisolution <rohit.ai.solution@gmail.com>
2026-03-18 16:33:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    const position = card.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 50) {
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
